a5f2ddc82c39f1528ba5c44d6b58062923b3eb4f
MadsenAMS <andre-madsen@hotmail.com>
2022-01-04 06:09:18 -0300

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+function Start() { 
+
+    $("#start").hide();
+
+    $("#gameBackground").append("<div id='player' class='anima1'></div>");
+	$("#gameBackground").append("<div id='enemy1' class='anima2'></div>");
+	$("#gameBackground").append("<div id='enemy2'></div>");
+	$("#gameBackground").append("<div id='friend' class='anima3'></div>");
+
+}
